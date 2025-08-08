5e5fc60af87ab84dd8b1c2c4cd25b9f817494dba
ahmadawal <awaludinahmad14@gmail.com>
2025-08-08 14:55:29 +0700

    Edit backend address

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 3000,
-    host: '0.0.0.0', // Enable LAN access
+    // port: 3000,
+    // host: '0.0.0.0', // Enable LAN access
     proxy: {
       '/api': {
         target: 'http://wo-mt.jembo.com:5000',
-        changeOrigin: true
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
       }
     }
   }
