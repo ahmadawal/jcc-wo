b9c24ef8e929678b9b7c9f90796cab57104496c8
ahmadawal <awaludinahmad14@gmail.com>
2025-08-08 14:34:30 +0700

    Edit backend address

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     host: '0.0.0.0', // Enable LAN access
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://wo-mt.jembo.com:5000',
         changeOrigin: true
       }
     }
