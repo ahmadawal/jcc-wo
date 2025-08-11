1800cc597f64412539cac7633284a3813addb4ed
ahmadawal <awaludinahmad14@gmail.com>
2025-08-11 10:09:19 +0700

    Edit Dockerfile

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,12 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   server: {
-    // port: 3000,
+    port: 3000,
     // host: '192.168.10.15', // Enable LAN access
     proxy: {
       '/api': {
-        target: 'http://wo-mt.jembo.com:5000',
+        // target: 'http://wo-mt.jembo.com:5000',
+        target: 'http://localhost:5000',
         changeOrigin: true,
         // rewrite: path => path.replace(/^\/api/, '')
       }
