15cb6eb7dfc712ff50bb8cef71b3706310a18789
ahmadawal <awaludinahmad14@gmail.com>
2025-08-18 19:35:54 +0700

    Update database connection

--- client/vite.config.js
+++ client/vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
         changeOrigin: true,
         secure: false,
         credentials: true,
-        // rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
